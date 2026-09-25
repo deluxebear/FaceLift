Allwayz/AirCard

fix: read unified device logs for Wallet card detection
Use os_trace_relay instead of the legacy syslog_relay service, which
omits the Info/Debug resource lookups containing Wallet card paths on
iOS 18.6.2. Decode framed activity records through MobileDevice and
preserve multiline messages for the existing card filters.

Display scanner diagnostics, drain the output through EOF, and reset
scan state after helper termination. Guard late callbacks when a scan
is stopped or replaced. Add protocol regression coverage and English
troubleshooting and community testing instructions.

Verified with:
- iPhone 15 Pro (iPhone16,1), iOS 18.6.2
- MacBook Air (M3, 2024), macOS 26.6.2
- AirCard 1.2.3 source baseline 02b5ba8

Live scanning detected eight identifiers matching the reference capture.
Helper-exit recovery and scan restart passed. Five unittest cases and
the universal arm64/x86_64 app build passed. No artwork was flashed.
The originally reported macOS 26.2 and issue #28's iOS 27 were not tested.

Related to Mak5er/AirCard#28

Sources/device_helper.m
@@ -5,6 +5,7 @@
 #include <unistd.h>
 
 #import "airlift_target.h"
+#import "os_trace.h"
 
 typedef const void *AMDeviceRef;
 typedef const void *AMDeviceNotificationRef;
@@ -49,6 +50,8 @@ extern int AMDServiceConnectionInvalidate(AMDServiceConnectionRef connection);
 extern int AMDServiceConnectionSend(AMDServiceConnectionRef connection,
                                     const void *bytes,
                                     size_t length);
+extern long AMDServiceConnectionReceive(AMDServiceConnectionRef connection,
+                                        void *bytes, long length);
 extern int AMDServiceConnectionSendMessage(AMDServiceConnectionRef connection,
                                            CFTypeRef message,
                                            CFPropertyListFormat format);
@@ -233,39 +236,89 @@ static int ListDevices(void) {
     return status == 0 ? 0 : 2;
 }
 
+static int StreamDeviceLogs(AMDServiceConnectionRef connection) {
+    // syslog_relay omits the Info/Debug resource lookups containing Wallet card
+    // identifiers on iOS 18. Request the unified activity stream instead.
+    NSDictionary *request = @{
+        @"Request": @"StartActivity",
+        @"Pid": @(UINT32_MAX),
+        @"MessageFilter": @0xFFFF,
+        @"StreamFlags": @0x3C, // Payload, historical, callstack and debug events.
+    };
+    if (AMDServiceConnectionSendMessage(connection,
+            (__bridge CFDictionaryRef)request, kCFPropertyListBinaryFormat_v1_0) != 0) {
+        fprintf(stderr, "AirCard scanner: Could not request device log streaming.\n");
+        return 2;
+    }
+
+    uint8_t type = 0;
+    NSString *error = nil;
+    NSData *reply = AirCardTraceReadFrame(AMDServiceConnectionReceive, connection,
+                                         &type, &error);
+    id status = reply && type == 1
+        ? [NSPropertyListSerialization propertyListWithData:reply
+              options:NSPropertyListImmutable format:NULL error:NULL] : nil;
+    if (![status isKindOfClass:NSDictionary.class] ||
+        ![status[@"Status"] isEqual:@"RequestSuccessful"]) {
+        fprintf(stderr, "AirCard scanner: %s\n",
+                (error ?: @"The device refused to start log streaming.").UTF8String);
+        return 2;
+    }
+
+    fprintf(stderr, "AirCard scanner: Connected to the unified device log stream.\n");
+    while (YES) {
+        @autoreleasepool {
+            NSData *record = AirCardTraceReadFrame(AMDServiceConnectionReceive,
+                                                   connection, &type, &error);
+            if (!record) {
+                fprintf(stderr, "AirCard scanner: %s\n", error.UTF8String);
+                return 2;
+            }
+            if (type != 2) continue;
+            NSString *line = AirCardTraceLogLine(record);
+            if (line) {
+                NSData *data = [line dataUsingEncoding:NSUTF8StringEncoding];
+                if (fwrite(data.bytes, 1, data.length, stdout) != data.length ||
+                    fflush(stdout) != 0) return 2;
+            }
+        }
+    }
+}
+
 static int RunSyslog(void) {
-    if (FindTarget() != 0 || !TargetDevice) return 2;
+    if (FindTarget() != 0 || !TargetDevice) {
+        fprintf(stderr, "AirCard scanner: iPhone not found. Reconnect it via USB.\n");
+        return 2;
+    }
     AMDeviceRef device = TargetDevice;
-    if (AMDeviceConnect(device) != 0) return 2;
+    if (AMDeviceConnect(device) != 0) {
+        fprintf(stderr, "AirCard scanner: Could not connect to the iPhone.\n");
+        return 2;
+    }
     if (!AMDeviceIsPaired(device)) AMDevicePair(device);
     if (AMDeviceValidatePairing(device) != 0 || AMDeviceStartSession(device) != 0) {
+        fprintf(stderr, "AirCard scanner: Unlock the iPhone and trust this Mac, then retry.\n");
         AMDeviceDisconnect(device);
         return 2;
     }
 
     AMDServiceConnectionRef connection = NULL;
     if (AMDeviceSecureStartService(
-            device, CFSTR("com.apple.syslog_relay"), NULL, &connection) != 0 ||
+            device, CFSTR("com.apple.os_trace_relay"), NULL, &connection) != 0 ||
         !connection) {
+        fprintf(stderr, "AirCard scanner: Could not open the device log service. Unlock the iPhone and retry.\n");
         AMDeviceStopSession(device);
         AMDeviceDisconnect(device);
         return 2;
     }
 
     signal(SIGPIPE, SIG_IGN);
-    int sock = AMDServiceConnectionGetSocket(connection);
-    char buffer[65536];
-    while (sock >= 0) {
-        ssize_t received = recv(sock, buffer, sizeof(buffer), 0);
-        if (received <= 0) break;
-        fwrite(buffer, 1, (size_t)received, stdout);
-        fflush(stdout);
-    }
+    int status = StreamDeviceLogs(connection);
 
     AMDServiceConnectionInvalidate(connection);
     AMDeviceStopSession(device);
     AMDeviceDisconnect(device);
-    return 0;
+    return status;
 }
 
 static void OpenSession(DeviceSession *session) {

tests/test_card_scanner.py
@@ -1,0 +1,46 @@
+import subprocess
+import sys
+import tempfile
+import unittest
+from pathlib import Path
+
+from aircard import CARD_REGEXES
+
+
+@unittest.skipUnless(sys.platform == "darwin", "The native log reader requires macOS")
+class CardScannerTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.temporary = tempfile.TemporaryDirectory()
+        cls.addClassCleanup(cls.temporary.cleanup)
+        cls.executable = Path(cls.temporary.name) / "test_os_trace"
+        source = Path(__file__).with_name("test_os_trace.m")
+        subprocess.run(
+            ["xcrun", "clang", "-fobjc-arc", "-Wall", "-Wextra", "-Werror",
+             "-framework", "Foundation", str(source), "-o", str(cls.executable)],
+            check=True, capture_output=True, text=True,
+        )
+
+    def test_activity_stream_protocol(self):
+        result = subprocess.run(
+            [str(self.executable)], check=True, capture_output=True, text=True,
+        )
+        self.assertIn("malformed records passed", result.stdout)
+
+    def test_ios18_resource_path_reaches_existing_card_patterns(self):
+        result = subprocess.run(
+            [str(self.executable), "--fixture"], check=True,
+            capture_output=True, text=True,
+        )
+        hashes = {
+            match.group(1)
+            for line in result.stdout.splitlines()
+            if "/cards/" in line.lower()
+            for pattern in CARD_REGEXES
+            for match in pattern.finditer(line)
+        }
+        self.assertEqual(hashes, {"AAAAAAAAAAAAAAAAAAAAAAAAAAA="})
+
+
+if __name__ == "__main__":
+    unittest.main()

deluxebear/FaceLift

(cherry picked from commit 97f4a99e5380218312219de38242ca2c4b3f1dc6)

Sources/device_helper.m
@@ -7,6 +7,7 @@
 #include <unistd.h>
 
 #import "airlift_target.h"
+#import "os_trace.h"
 
 typedef const void *AMDeviceRef;
 typedef const void *AMDeviceNotificationRef;
@@ -53,6 +54,8 @@ extern int AMDServiceConnectionInvalidate(AMDServiceConnectionRef connection);
 extern int AMDServiceConnectionSend(AMDServiceConnectionRef connection,
                                     const void *bytes,
                                     size_t length);
+extern long AMDServiceConnectionReceive(AMDServiceConnectionRef connection,
+                                        void *bytes, long length);
 extern int AMDServiceConnectionSendMessage(AMDServiceConnectionRef connection,
                                            CFTypeRef message,
                                            CFPropertyListFormat format);
@@ -346,39 +349,89 @@ static int ListDevices(void) {
     return status == 0 ? 0 : 2;
 }
 
+static int StreamDeviceLogs(AMDServiceConnectionRef connection) {
+    // syslog_relay omits the Info/Debug resource lookups containing Wallet card
+    // identifiers on iOS 18. Request the unified activity stream instead.
+    NSDictionary *request = @{
+        @"Request": @"StartActivity",
+        @"Pid": @(UINT32_MAX),
+        @"MessageFilter": @0xFFFF,
+        @"StreamFlags": @0x3C, // Payload, historical, callstack and debug events.
+    };
+    if (AMDServiceConnectionSendMessage(connection,
+            (__bridge CFDictionaryRef)request, kCFPropertyListBinaryFormat_v1_0) != 0) {
+        fprintf(stderr, "FaceLift scanner: Could not request device log streaming.\n");
+        return 2;
+    }
+
+    uint8_t type = 0;
+    NSString *error = nil;
+    NSData *reply = AirCardTraceReadFrame(AMDServiceConnectionReceive, connection,
+                                         &type, &error);
+    id status = reply && type == 1
+        ? [NSPropertyListSerialization propertyListWithData:reply
+              options:NSPropertyListImmutable format:NULL error:NULL] : nil;
+    if (![status isKindOfClass:NSDictionary.class] ||
+        ![status[@"Status"] isEqual:@"RequestSuccessful"]) {
+        fprintf(stderr, "FaceLift scanner: %s\n",
+                (error ?: @"The device refused to start log streaming.").UTF8String);
+        return 2;
+    }
+
+    fprintf(stderr, "FaceLift scanner: Connected to the unified device log stream.\n");
+    while (YES) {
+        @autoreleasepool {
+            NSData *record = AirCardTraceReadFrame(AMDServiceConnectionReceive,
+                                                   connection, &type, &error);
+            if (!record) {
+                fprintf(stderr, "FaceLift scanner: %s\n", error.UTF8String);
+                return 2;
+            }
+            if (type != 2) continue;
+            NSString *line = AirCardTraceLogLine(record);
+            if (line) {
+                NSData *data = [line dataUsingEncoding:NSUTF8StringEncoding];
+                if (fwrite(data.bytes, 1, data.length, stdout) != data.length ||
+                    fflush(stdout) != 0) return 2;
+            }
+        }
+    }
+}
+
 static int RunSyslog(void) {
-    if (FindTarget() != 0 || !TargetDevice) return 2;
+    if (FindTarget() != 0 || !TargetDevice) {
+        fprintf(stderr, "FaceLift scanner: iPhone not found. Reconnect it via USB.\n");
+        return 2;
+    }
     AMDeviceRef device = TargetDevice;
-    if (AMDeviceConnect(device) != 0) return 2;
+    if (AMDeviceConnect(device) != 0) {
+        fprintf(stderr, "FaceLift scanner: Could not connect to the iPhone.\n");
+        return 2;
+    }
     if (!AMDeviceIsPaired(device)) AMDevicePair(device);
     if (AMDeviceValidatePairing(device) != 0 || AMDeviceStartSession(device) != 0) {
+        fprintf(stderr, "FaceLift scanner: Unlock the iPhone and trust this Mac, then retry.\n");
         AMDeviceDisconnect(device);
         return 2;
     }
 
     AMDServiceConnectionRef connection = NULL;
     if (AMDeviceSecureStartService(
-            device, CFSTR("com.apple.syslog_relay"), NULL, &connection) != 0 ||
+            device, CFSTR("com.apple.os_trace_relay"), NULL, &connection) != 0 ||
         !connection) {
+        fprintf(stderr, "FaceLift scanner: Could not open the device log service. Unlock the iPhone and retry.\n");
         AMDeviceStopSession(device);
         AMDeviceDisconnect(device);
         return 2;
     }
 
     signal(SIGPIPE, SIG_IGN);
-    int sock = AMDServiceConnectionGetSocket(connection);
-    char buffer[65536];
-    while (sock >= 0) {
-        ssize_t received = recv(sock, buffer, sizeof(buffer), 0);
-        if (received <= 0) break;
-        fwrite(buffer, 1, (size_t)received, stdout);
-        fflush(stdout);
-    }
+    int status = StreamDeviceLogs(connection);
 
     AMDServiceConnectionInvalidate(connection);
     AMDeviceStopSession(device);
     AMDeviceDisconnect(device);
-    return 0;
+    return status;
 }
 
 static void OpenSession(DeviceSession *session) {

tests/test_card_scanner.py
@@ -1,0 +1,48 @@
+import subprocess
+import sys
+import tempfile
+import unittest
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+from facelift import CARD_REGEXES
+
+
+@unittest.skipUnless(sys.platform == "darwin", "The native log reader requires macOS")
+class CardScannerTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.temporary = tempfile.TemporaryDirectory()
+        cls.addClassCleanup(cls.temporary.cleanup)
+        cls.executable = Path(cls.temporary.name) / "test_os_trace"
+        source = Path(__file__).with_name("test_os_trace.m")
+        subprocess.run(
+            ["xcrun", "clang", "-fobjc-arc", "-Wall", "-Wextra", "-Werror",
+             "-framework", "Foundation", str(source), "-o", str(cls.executable)],
+            check=True, capture_output=True, text=True,
+        )
+
+    def test_activity_stream_protocol(self):
+        result = subprocess.run(
+            [str(self.executable)], check=True, capture_output=True, text=True,
+        )
+        self.assertIn("malformed records passed", result.stdout)
+
+    def test_ios18_resource_path_reaches_existing_card_patterns(self):
+        result = subprocess.run(
+            [str(self.executable), "--fixture"], check=True,
+            capture_output=True, text=True,
+        )
+        hashes = {
+            match.group(1)
+            for line in result.stdout.splitlines()
+            if "/cards/" in line.lower()
+            for pattern in CARD_REGEXES
+            for match in pattern.finditer(line)
+        }
+        self.assertEqual(hashes, {"AAAAAAAAAAAAAAAAAAAAAAAAAAA="})
+
+
+if __name__ == "__main__":
+    unittest.main()