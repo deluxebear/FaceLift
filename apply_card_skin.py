Allwayz/AirCard

fix: invalidate stale Wallet artwork on iOS 27

apply_card_skin.py
@@ -344,22 +344,94 @@ def write_files_batch(
     return False
 
 
+def remove_files(udid: str, target: str, leaves: list[str], retries: int = 3) -> bool:
+    """Remove specific files through the relocated Airlift symlink.
+
+    Wallet only rebuilds its rendered card faces when the old cache entries are
+    absent. Overwriting them with arbitrary bytes leaves stale artwork active on
+    recent iOS releases, so cache invalidation must be a real unlink operation.
+    """
+    if not leaves:
+        return True
+    if any(not leaf or "/" in leaf or leaf in {".", ".."} for leaf in leaves):
+        raise ValueError("cache leaves must be plain file names")
+
+    for attempt in range(1, max(1, retries) + 1):
+        try:
+            token = secrets.token_hex(10)
+            source = f"{SOURCE_PREFIX}{token}"
+            link_destination = f"{LINK_PREFIX}{token}"
+            recovered = f"{RECOVERED_PREFIX}{token}"
+            link_identifier = f"../../{source}/p0/p1/p2/link"
+            protected_identifiers = [
+                f"../../{link_destination}/{leaf}" for leaf in leaves
+            ]
+            removed_destinations = [
+                f"{source}/removed-{index}" for index in range(len(leaves))
+            ]
+
+            with tempfile.TemporaryDirectory(prefix="airlift-remove-") as temporary:
+                work = Path(temporary)
+                archive_path = work / "payload.zip"
+                books_path = work / "Books.plist"
+                snapshot_root = work / "books-snapshot"
+                snapshot_root.mkdir()
+
+                # Relocate the symlink first, then have AirTraffic move each
+                # protected cache file out through it. This is a real unlink;
+                # AFCRemovePath cannot traverse the protected link on iOS 27.
+                archive_path.write_bytes(build_archive(target, b"aircard-v2"))
+                books_path.write_bytes(build_books(
+                    [link_identifier, *protected_identifiers]
+                ))
+
+                snapshot = native("snapshot-books", udid, os.fspath(snapshot_root))
+                if not operation_ok(snapshot):
+                    raise RuntimeError("could not snapshot Books state")
+                stage = native(
+                    "stage", udid, source, link_destination, recovered,
+                    os.fspath(archive_path), os.fspath(books_path),
+                    os.fspath(snapshot_root),
+                )
+                if not operation_ok(stage):
+                    raise RuntimeError("could not stage cache removal")
+
+                atc = run_json(
+                    [os.fspath(AIRTRAFFIC_HOST), udid,
+                     link_identifier, link_destination,
+                     *[part for pair in zip(protected_identifiers, removed_destinations)
+                       for part in pair]],
+                    timeout=120,
+                )
+                if atc.get("exitCode") != 0 or not atc.get("ok"):
+                    native("finish-write", udid, source, link_destination,
+                           recovered, os.fspath(snapshot_root))
+                    raise RuntimeError("could not relocate cache link")
+
+                finish = native(
+                    "finish-moved-removal", udid, source, link_destination,
+                    recovered, os.fspath(snapshot_root), str(len(leaves)),
+                )
+                if operation_ok(finish):
+                    return True
+        except Exception:
+            pass
+        if attempt < retries:
+            time.sleep(0.4 * attempt)
+    return False
+
+
 def invalidate_cache(udid: str, card_hash: str) -> bool:
-    """Invalidates card image cache by corrupting cache leaves in .cache and .pkcache."""
-    any_ok = False
-    cache_leaves = [("FrontFace", b"corrupted"), ("PlaceHolder", b"corrupted"), ("Preview", b"corrupted")]
+    """Remove every rendered card face so Wallet must rebuild from the pass."""
+    all_ok = True
+    cache_leaves = ["FrontFace", "PlaceHolder", "Preview"]
     for ext in [".cache", ".pkcache"]:
         cache_dir = f"/var/mobile/Library/Passes/Cards/{card_hash}{ext}"
         try:
-            if write_files_batch(udid, cache_dir, cache_leaves):
-                any_ok = True
-            else:
-                for leaf, payload in cache_leaves:
-                    if write_file(udid, cache_dir, leaf, payload):
-                        any_ok = True
+            all_ok = remove_files(udid, cache_dir, cache_leaves) and all_ok
         except Exception:
-            pass
-    return any_ok
+            all_ok = False
+    return all_ok
 
 
 def main():

tests/test_card_flash.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from unittest.mock import Mock, patch
 
 import aircard_backend
+import apply_card_skin
 
 
 PNG_1X1 = base64.b64decode(
@@ -17,14 +18,37 @@ PNG_1X1 = base64.b64decode(
 
 
 class CardFlashTests(unittest.TestCase):
-    def test_flash_writes_pdf_and_invalidates_placeholder(self) -> None:
+    def test_cache_removal_moves_link_and_required_companion_payload(self) -> None:
+        successful = {
+            "exitCode": 0,
+            "targetGatePassed": True,
+            "operation": {"ok": True},
+        }
+        with (
+            patch.object(apply_card_skin, "native", return_value=successful),
+            patch.object(apply_card_skin, "run_json", return_value={"exitCode": 0, "ok": True}) as transfer,
+        ):
+            result = apply_card_skin.remove_files(
+                "device", "/protected/card.cache", ["FrontFace"], retries=1
+            )
+
+        self.assertTrue(result)
+        command = transfer.call_args.args[0]
+        self.assertEqual(len(command), 6)
+        self.assertIn("/airlift-link-", command[4])
+        self.assertTrue(command[5].endswith("/removed-0"))
+
+    def test_flash_writes_pdf_and_removes_rendered_cache(self) -> None:
         with tempfile.TemporaryDirectory() as temporary:
             image_path = Path(temporary) / "card.png"
             image_path.write_bytes(PNG_1X1)
             write_file = Mock(return_value=True)
+            remove_files = Mock(return_value=True)
 
             with (
                 patch.object(aircard_backend, "write_file", write_file),
+                patch.object(aircard_backend, "write_files_batch", Mock(return_value=False)),
+                patch.object(aircard_backend, "remove_files", remove_files),
                 redirect_stdout(io.StringIO()),
             ):
                 result = aircard_backend.cmd_flash("device", "card", str(image_path))
@@ -47,13 +71,24 @@ class CardFlashTests(unittest.TestCase):
         )
         self.assertTrue(pass_assets["cardBackgroundCombined.pdf"].startswith(b"%PDF-"))
 
-        cache_entries = {(target, leaf) for _, target, leaf, _ in writes}
+        removals = [call.args for call in remove_files.call_args_list]
         for extension in (".cache", ".pkcache"):
-            for leaf in aircard_backend.CACHE_FILES:
-                self.assertIn(
-                    (f"/var/mobile/Library/Passes/Cards/card{extension}", leaf),
-                    cache_entries,
-                )
+            self.assertIn(("device", f"/var/mobile/Library/Passes/Cards/card{extension}", list(aircard_backend.CACHE_FILES)), removals)
+
+    def test_flash_fails_when_wallet_cache_cannot_be_removed(self) -> None:
+        with tempfile.TemporaryDirectory() as temporary:
+            image_path = Path(temporary) / "card.png"
+            image_path.write_bytes(PNG_1X1)
+            output = io.StringIO()
+            with (
+                patch.object(aircard_backend, "write_files_batch", return_value=True),
+                patch.object(aircard_backend, "remove_files", side_effect=[True, False]),
+                redirect_stdout(output),
+            ):
+                result = aircard_backend.cmd_flash("device", "card", str(image_path))
+        messages = [json.loads(line) for line in output.getvalue().splitlines()]
+        self.assertFalse(result)
+        self.assertEqual(messages[-1]["type"], "error")
 
     def test_flash_reports_failure_when_an_asset_write_fails(self) -> None:
         with tempfile.TemporaryDirectory() as temporary:

deluxebear/FaceLift

(cherry picked from commit 7e8979b7a701435531935ed4b45a9444adbec572)

apply_card_skin.py
@@ -685,24 +685,94 @@ def _write_preview(payload: bytes, dest: Path) -> None:
             dest.write_bytes(png_path.read_bytes())
         return
     dest.write_bytes(payload)
+def remove_files(udid: str, target: str, leaves: list[str], retries: int = 3) -> bool:
+    """Remove specific files through the relocated Airlift symlink.
+
+    Wallet only rebuilds its rendered card faces when the old cache entries are
+    absent. Overwriting them with arbitrary bytes leaves stale artwork active on
+    recent iOS releases, so cache invalidation must be a real unlink operation.
+    """
+    if not leaves:
+        return True
+    if any(not leaf or "/" in leaf or leaf in {".", ".."} for leaf in leaves):
+        raise ValueError("cache leaves must be plain file names")
+
+    for attempt in range(1, max(1, retries) + 1):
+        try:
+            token = secrets.token_hex(10)
+            source = f"{SOURCE_PREFIX}{token}"
+            link_destination = f"{LINK_PREFIX}{token}"
+            recovered = f"{RECOVERED_PREFIX}{token}"
+            link_identifier = f"../../{source}/p0/p1/p2/link"
+            protected_identifiers = [
+                f"../../{link_destination}/{leaf}" for leaf in leaves
+            ]
+            removed_destinations = [
+                f"{source}/removed-{index}" for index in range(len(leaves))
+            ]
+
+            with tempfile.TemporaryDirectory(prefix="airlift-remove-") as temporary:
+                work = Path(temporary)
+                archive_path = work / "payload.zip"
+                books_path = work / "Books.plist"
+                snapshot_root = work / "books-snapshot"
+                snapshot_root.mkdir()
+
+                # Relocate the symlink first, then have AirTraffic move each
+                # protected cache file out through it. This is a real unlink;
+                # AFCRemovePath cannot traverse the protected link on iOS 27.
+                archive_path.write_bytes(build_archive(target, b"facelift-v2"))
+                books_path.write_bytes(build_books(
+                    [link_identifier, *protected_identifiers]
+                ))
+
+                snapshot = native("snapshot-books", udid, os.fspath(snapshot_root))
+                if not operation_ok(snapshot):
+                    raise RuntimeError("could not snapshot Books state")
+                stage = native(
+                    "stage", udid, source, link_destination, recovered,
+                    os.fspath(archive_path), os.fspath(books_path),
+                    os.fspath(snapshot_root),
+                )
+                if not operation_ok(stage):
+                    raise RuntimeError("could not stage cache removal")
+
+                atc = run_json(
+                    [os.fspath(AIRTRAFFIC_HOST), udid,
+                     link_identifier, link_destination,
+                     *[part for pair in zip(protected_identifiers, removed_destinations)
+                       for part in pair]],
+                    timeout=120,
+                )
+                if atc.get("exitCode") != 0 or not atc.get("ok"):
+                    native("finish-write", udid, source, link_destination,
+                           recovered, os.fspath(snapshot_root))
+                    raise RuntimeError("could not relocate cache link")
+
+                finish = native(
+                    "finish-moved-removal", udid, source, link_destination,
+                    recovered, os.fspath(snapshot_root), str(len(leaves)),
+                )
+                if operation_ok(finish):
+                    return True
+        except Exception:
+            pass
+        if attempt < retries:
+            time.sleep(0.4 * attempt)
+    return False
 
 
 def invalidate_cache(udid: str, card_hash: str) -> bool:
-    """Invalidates card image cache by corrupting cache leaves in .cache and .pkcache."""
-    any_ok = False
-    cache_leaves = [("FrontFace", b"corrupted"), ("PlaceHolder", b"corrupted"), ("Preview", b"corrupted")]
+    """Remove every rendered card face so Wallet must rebuild from the pass."""
+    all_ok = True
+    cache_leaves = ["FrontFace", "PlaceHolder", "Preview"]
     for ext in [".cache", ".pkcache"]:
         cache_dir = f"/var/mobile/Library/Passes/Cards/{card_hash}{ext}"
         try:
-            if write_files_batch(udid, cache_dir, cache_leaves):
-                any_ok = True
-            else:
-                for leaf, payload in cache_leaves:
-                    if write_file(udid, cache_dir, leaf, payload):
-                        any_ok = True
+            all_ok = remove_files(udid, cache_dir, cache_leaves) and all_ok
         except Exception:
-            pass
-    return any_ok
+            all_ok = False
+    return all_ok
 
 
 def main():

tests/test_card_flash.py
@@ -34,14 +34,37 @@ class CardFlashTests(unittest.TestCase):
         self._patch.stop()
         self._support.cleanup()
 
-    def test_flash_writes_pdf_and_invalidates_placeholder(self) -> None:
+    def test_cache_removal_moves_link_and_required_companion_payload(self) -> None:
+        successful = {
+            "exitCode": 0,
+            "targetGatePassed": True,
+            "operation": {"ok": True},
+        }
+        with (
+            patch.object(apply_card_skin, "native", return_value=successful),
+            patch.object(apply_card_skin, "run_json", return_value={"exitCode": 0, "ok": True}) as transfer,
+        ):
+            result = apply_card_skin.remove_files(
+                "device", "/protected/card.cache", ["FrontFace"], retries=1
+            )
+
+        self.assertTrue(result)
+        command = transfer.call_args.args[0]
+        self.assertEqual(len(command), 6)
+        self.assertIn("/airlift-link-", command[4])
+        self.assertTrue(command[5].endswith("/removed-0"))
+
+    def test_flash_writes_pdf_and_removes_rendered_cache(self) -> None:
         with tempfile.TemporaryDirectory() as temporary:
             image_path = Path(temporary) / "card.png"
             image_path.write_bytes(PNG_1X1)
             write_file = Mock(return_value=True)
+            remove_files = Mock(return_value=True)
 
             with (
                 patch.object(facelift_backend, "write_file", write_file),
+                patch.object(facelift_backend, "write_files_batch", Mock(return_value=False)),
+                patch.object(facelift_backend, "remove_files", remove_files),
                 redirect_stdout(io.StringIO()),
             ):
                 result = facelift_backend.cmd_flash(UDID, CARD, str(image_path))
@@ -64,13 +87,24 @@ class CardFlashTests(unittest.TestCase):
         )
         self.assertTrue(pass_assets["cardBackgroundCombined.pdf"].startswith(b"%PDF-"))
 
-        cache_entries = {(target, leaf) for _, target, leaf, _ in writes}
+        removals = [call.args for call in remove_files.call_args_list]
         for extension in (".cache", ".pkcache"):
-            for leaf in facelift_backend.CACHE_FILES:
-                self.assertIn(
-                    (f"/var/mobile/Library/Passes/Cards/{CARD}{extension}", leaf),
-                    cache_entries,
-                )
+            self.assertIn((UDID, f"/var/mobile/Library/Passes/Cards/{CARD}{extension}", list(facelift_backend.CACHE_FILES)), removals)
+
+    def test_flash_fails_when_wallet_cache_cannot_be_removed(self) -> None:
+        with tempfile.TemporaryDirectory() as temporary:
+            image_path = Path(temporary) / "card.png"
+            image_path.write_bytes(PNG_1X1)
+            output = io.StringIO()
+            with (
+                patch.object(facelift_backend, "write_files_batch", return_value=True),
+                patch.object(facelift_backend, "remove_files", side_effect=[True, False]),
+                redirect_stdout(output),
+            ):
+                result = facelift_backend.cmd_flash(UDID, CARD, str(image_path))
+        messages = [json.loads(line) for line in output.getvalue().splitlines()]
+        self.assertFalse(result)
+        self.assertEqual(messages[-1]["type"], "error")
 
     def test_flash_reports_failure_when_an_asset_write_fails(self) -> None:
         with tempfile.TemporaryDirectory() as temporary: